c752a695562f14890ea0efd1d2bc44bad92377f1
Frederico F Silva <fredericoffs@live.com>
2021-05-27 20:12:47 -0300

    Consulta simples com @Query e query methods

--- src/main/java/com/fredericoffs/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fredericoffs/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,19 @@
 package com.fredericoffs.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fredericoffs.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchByTitle(String text);
 
 }
