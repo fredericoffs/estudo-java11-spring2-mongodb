6e36fa682a9cdbff0045537805c6e8df03f22a8a
Frederico F Silva <fredericoffs@live.com>
2021-05-27 17:07:34 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/fredericoffs/workshopmongo/config/Instantiation.java
+++ src/main/java/com/fredericoffs/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.fredericoffs.workshopmongo.domain.Post;
 import com.fredericoffs.workshopmongo.domain.User;
 import com.fredericoffs.workshopmongo.dto.AuthorDTO;
+import com.fredericoffs.workshopmongo.dto.CommentDTO;
 import com.fredericoffs.workshopmongo.repository.PostRepository;
 import com.fredericoffs.workshopmongo.repository.UserRepository;
 
@@ -34,8 +35,16 @@ public class Instantiation implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post p1 = new Post(null, Instant.parse("2018-03-21T19:53:07Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post p1 = new Post(null, Instant.parse("2018-03-21T19:53:07Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
 		Post p2 = new Post(null, Instant.now(), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", Instant.parse("2018-03-21T19:53:07Z"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", Instant.parse("2018-03-22T19:53:07Z"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", Instant.parse("2018-03-23T19:53:07Z"), new AuthorDTO(alex));
+
+		p1.getComments().addAll(Arrays.asList(c1, c2));
+		p2.getComments().addAll(Arrays.asList(c3));
 
 		postRepository.saveAll(Arrays.asList(p1, p2));
 
--- src/main/java/com/fredericoffs/workshopmongo/domain/Post.java
+++ src/main/java/com/fredericoffs/workshopmongo/domain/Post.java
@@ -2,12 +2,15 @@ package com.fredericoffs.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fredericoffs.workshopmongo.dto.AuthorDTO;
+import com.fredericoffs.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,6 +24,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -72,6 +77,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
