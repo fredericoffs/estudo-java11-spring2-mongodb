b39098908db69d31bd28249e9b7780b8672e9ec9
Frederico F Silva <fredericoffs@live.com>
2021-05-27 16:16:24 -0300

    Obtendo um post por id

--- src/main/java/com/fredericoffs/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fredericoffs/workshopmongo/resources/UserResource.java
@@ -62,12 +62,10 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@GetMapping(value = "/{id}/posts")
 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj.getPost());
 	}
-	
-
 }
